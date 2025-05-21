86f3d8abe5a03b97e01a0b9cda73b2ba6b271763
teerapat.jun <teerapat.jun@aksorn.com>
2025-05-21 15:29:13 +0700

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   return (
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1>ทดสอบ 1</h1>
         <AudioSyncTextEditor
           params={{
             heightEditor: 524,
