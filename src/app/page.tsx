3d001515211ce1594dc983f737b49108239a35d8
teerapat.jun <teerapat.jun@aksorn.com>
2025-05-21 11:31:13 +0700

    update icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   console.log("highlightedRanges", highlightedRanges);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <AudioSyncTextEditor
           params={{
